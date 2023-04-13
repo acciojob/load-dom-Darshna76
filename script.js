5beec21f1df2aa1a9f4f1efee96b7681441f81cf
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-04-13 13:52:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  const message = 'DOM load success';
+  const p = document.createElement('p');
+  p.textContent = message;
+  document.body.appendChild(p);
+});
